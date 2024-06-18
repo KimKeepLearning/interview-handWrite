967a110de1a1f83cba8a897d42acd5df98538de6
yhuang <18261195537@163.com>
2024-06-18 16:12:54 +0800

    feat: moxie

--- dist/tmp.dev.js
+++ dist/tmp.dev.js
@@ -1,8 +1,128 @@
 "use strict";
 
-function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+function _toConsumableArray(arr) { return _arrayWithoutHoles(arr) || _iterableToArray(arr) || _nonIterableSpread(); }
 
-var creatSingleton = function () {
+function _nonIterableSpread() { throw new TypeError("Invalid attempt to spread non-iterable instance"); }
+
+function _iterableToArray(iter) { if (Symbol.iterator in Object(iter) || Object.prototype.toString.call(iter) === "[object Arguments]") return Array.from(iter); }
+
+function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = new Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+var LRU =
+/*#__PURE__*/
+function () {
+  function LRU(size) {
+    _classCallCheck(this, LRU);
+
+    this.size = size;
+    this.data = new Map();
+  }
+
+  _createClass(LRU, [{
+    key: "set",
+    value: function set(key, value) {
+      var data = this.data,
+          size = this.size;
+
+      if (data.has(key)) {
+        data["delete"](key);
+      }
+
+      data.set(key, value);
+
+      if (data.size > size) {
+        var delKey = data.keys().next().value;
+        data["delete"](delKey);
+      }
+    }
+  }, {
+    key: "get",
+    value: function get(key) {
+      if (!this.data.has(key)) {
+        return null;
+      }
+
+      var value = this.data.get(key);
+      this.data.set(key, value);
+      return value;
+    }
+  }]);
+
+  return LRU;
+}();
+
+var setTimeOutMy = function setTimeOutMy(func, wait) {
+  var timer = null;
+  timer = setInterval(function () {
+    clearInterval(timer);
+    func();
+  }, wait);
+};
+
+var setIntervalMy = function setIntervalMy(func, wait) {
+  var timer = null;
+
+  function interval() {
+    func();
+    timer = setTimeOutMy(interval, wait);
+  }
+
+  timer = setTimeOutMy(interval, wait);
+  return {
+    clear: function clear() {
+      return clearTimeout(timer);
+    }
+  };
+};
+
+var EventEmitter =
+/*#__PURE__*/
+function () {
+  function EventEmitter() {
+    _classCallCheck(this, EventEmitter);
+
+    this.events = {};
+  }
+
+  _createClass(EventEmitter, [{
+    key: "on",
+    value: function on(eventName, callback) {
+      if (!this.events.eventName) {
+        this.events.eventName = [];
+      }
+
+      this.events[eventName].push(callback);
+    }
+  }, {
+    key: "off",
+    value: function off(eventName, cb) {
+      this.events[eventName] = this.events[eventName].filter(function (event) {
+        return event !== cb && event.init !== cb;
+      });
+    }
+  }, {
+    key: "once",
+    value: function once(eventName, cb) {
+      function one() {
+        cb();
+        this.off(eventName, one);
+      }
+
+      one.init = cb;
+      this.on(eventName, cb);
+    }
+  }]);
+
+  return EventEmitter;
+}();
+
+var createSingleton = function () {
   var instance = null;
   return function (name) {
     if (instance) {
@@ -15,12 +135,163 @@ var creatSingleton = function () {
   };
 }();
 
-var light = function light(func, wait) {
+var strategies = {
+  straA: function straA() {
+    return {
+      color: 'red',
+      bgColor: 'yellow'
+    };
+  },
+  straB: function straB() {
+    return {
+      color: 'red',
+      bgColor: 'yellow'
+    };
+  }
+};
+strategies[straA]();
+
+var Subject =
+/*#__PURE__*/
+function () {
+  function Subject(name) {
+    _classCallCheck(this, Subject);
+
+    this.name = name;
+    this.obs = [];
+  }
+
+  _createClass(Subject, [{
+    key: "observe",
+    value: function observe(o) {
+      this.obs.push(o);
+    }
+  }, {
+    key: "notice",
+    value: function notice() {
+      var _this = this;
+
+      this.obs.forEach(function (observer) {
+        observer.receive(_this);
+      });
+    }
+  }]);
+
+  return Subject;
+}();
+
+var Observer =
+/*#__PURE__*/
+function () {
+  function Observer(name) {
+    _classCallCheck(this, Observer);
+
+    this.name = name;
+  }
+
+  _createClass(Observer, [{
+    key: "receive",
+    value: function receive(subject) {}
+  }]);
+
+  return Observer;
+}();
+
+var pipe = function pipe(funcs) {
+  return funcs.reduce(function (f, g) {
+    return function () {
+      return f(g.apply(void 0, arguments));
+    };
+  });
+};
+
+var curry = function curry(func) {
+  var preArgs = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+  var arity = func.length;
+  return function () {
+    for (var _len = arguments.length, nextArgs = new Array(_len), _key = 0; _key < _len; _key++) {
+      nextArgs[_key] = arguments[_key];
+    }
+
+    var args = [].concat(_toConsumableArray(preArgs), nextArgs);
+
+    if (args.length === arity) {
+      return func.apply(void 0, _toConsumableArray(args));
+    }
+
+    return curry(func, args);
+  };
+};
+
+Promise.all = function (promises) {
+  return new Promise(function (resolve, reject) {});
+};
+
+var sleep = function sleep(wait) {
   return new Promise(function (resolve, reject) {
+    setTimeOut(function () {
+      resolve();
+    }, wait);
+  });
+};
+
+var requestPools = function requestPools(urls) {
+  var max = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 3;
+  var completed = 0;
+  var progress = 0;
+  var res = [];
+
+  var request = function request() {
+    var i = progress;
+    progress++;
+
+    if (i > urls.length) {
+      return;
+    }
+
+    fetch(urls[i]).then(function (val) {
+      res[i] = val;
+    })["catch"](function (e) {
+      res[i] = e;
+    })["finally"](function () {
+      completed++;
+
+      if (completed === urls.length) {
+        resolve(res);
+        return;
+      }
+
+      request();
+    });
+  };
+
+  while (progress < Math.min(urls.length, max)) {
+    request();
+  }
+};
+
+function red() {
+  console.log('red');
+}
+
+function green() {
+  console.log('green');
+}
+
+function yellow() {
+  console.log('yellow');
+} //红灯3秒亮一次，
+//黄灯2秒亮一次，
+//绿灯1秒亮一次；
+//如何让三个灯不断交替重复亮灯？（用Promise实现）三个亮灯函数已经存在：
+
+
+var light = function light(func, time) {
+  return new Promise(function (resolve) {
     setTimeout(function () {
       func();
       resolve();
-    }, wait);
+    }, time);
   });
 };
 
@@ -32,108 +303,16 @@ var step = function step() {
   }).then(function () {
     return light(yellow, 1000);
   }).then(function () {
-    step();
+    return step();
   });
 };
 
 step();
+/**
+ * 2024-6-18
+ * - 并发限制
+ */
 
-Promise.resolveMy = function (param) {
-  if (param instanceof Promise) {
-    return param;
-  }
-
-  return new Promise(function (resolve, reject) {
-    if (param.then && typeof param.then === 'function') {
-      param.then(resolve, reject);
-    } else {
-      resolve(param);
-    }
-  });
-};
-
-Promise.all = function (promises) {
-  return new Promise(function (resolve, reject) {
-    var len = promises.length;
-    var res = [];
-    var completed = 0;
-
-    if (!res.length) {
-      resolve(res);
-      return;
-    }
-
-    var _loop = function _loop(i) {
-      Promise.resolve(promises[i]).then(function (val) {
-        res[i] = val;
-        completed++;
-
-        if (completed === len) {
-          resolve(res);
-        }
-      })["catch"](function (e) {
-        reject(e);
-      });
-    };
-
-    for (var i = 0; i < len; i++) {
-      _loop(i);
-    }
-  });
-};
-
-var concurrencyPromise = function concurrencyPromise(urls, max) {
-  return new Promise(function (resolve, reject) {
-    var len = urls.length;
-    var completed = 0;
-    var current = 0;
-    var res = [];
-
-    if (!len) {
-      resolve(res);
-      return;
-    }
-
-    var request = function request() {
-      var i = current;
-      current++;
-
-      if (i >= len) {
-        return;
-      }
-
-      fetch(urls[i]).then(function (val) {
-        res[i] = val;
-      })["catch"](function (err) {
-        res[i] = err;
-      })["finally"](function () {
-        completed++;
-
-        if (completed >= len) {
-          resolve(res);
-          return;
-        }
-
-        request();
-      });
-    };
-
-    while (current <= Math.min(max, urls.length)) {
-      request();
-    }
-  });
-};
-
-var newMy = function newMy(cst) {
-  var newObj = Object.create(cst.prototype);
-
-  for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-    args[_key - 1] = arguments[_key];
-  }
-
-  var res = cst.apply(newObj, args);
-  return _typeof(res) === 'object' ? res : newObj;
-};
 /**
  * 2024-6-13
  * 单例模式
--- tmp.js
+++ tmp.js
@@ -1,4 +1,72 @@
-const creatSingleton = (function () {
+class LRU {
+  constructor(size) {
+    this.size = size;
+    this.data = new Map();
+  }
+  set(key, value) {
+    const { data, size } = this;
+    if (data.has(key)) {
+      data.delete(key);
+    }
+    data.set(key, value);
+    if (data.size > size) {
+      const delKey = data.keys().next().value;
+      data.delete(delKey);
+    }
+  }
+  get(key) {
+    if (!this.data.has(key)) {
+      return null;
+    }
+    const value = this.data.get(key);
+    this.data.set(key, value);
+    return value;
+  }
+}
+
+const setTimeOutMy = (func, wait) => {
+  let timer = null;
+  timer = setInterval(() => {
+    clearInterval(timer);
+    func();
+  }, wait)
+}
+const setIntervalMy = (func, wait) => {
+  let timer = null;
+  function interval() {
+    func();
+    timer = setTimeOutMy(interval, wait)
+  }
+  timer = setTimeOutMy(interval, wait)
+  return {
+    clear: () => clearTimeout(timer)
+  }
+}
+
+class EventEmitter {
+  constructor() {
+    this.events = {};
+  } 
+  on(eventName, callback) {
+    if (!this.events.eventName) {
+      this.events.eventName = []
+    }
+    this.events[eventName].push(callback)
+  }
+  off(eventName, cb) {
+    this.events[eventName] = this.events[eventName].filter(event => event !== cb && event.init !== cb)
+  }
+  once(eventName, cb) {
+    function one() {
+      cb();
+      this.off(eventName, one);
+    }
+    one.init = cb;
+    this.on(eventName, cb);
+  }
+}
+
+const createSingleton = (function () {
   let instance = null;
   return function (name) {
     if (instance) {
@@ -10,14 +78,126 @@ const creatSingleton = (function () {
   }
 })()
 
-const light = (func, wait) => {
+const strategies = {
+  straA() {
+    return {
+      color: 'red',
+      bgColor: 'yellow',
+    }
+  },
+  straB() {
+    return {
+      color: 'red',
+      bgColor: 'yellow',
+    }
+  }
+}
+strategies[straA]()
+
+class Subject {
+  constructor(name) {
+    this.name = name;
+    this.obs = [];
+  }
+  observe(o) {
+    this.obs.push(o)
+  }
+  notice() {
+    this.obs.forEach(observer => {
+      observer.receive(this);
+    })
+  }
+}
+
+class Observer {
+  constructor(name) {
+    this.name = name
+  }
+  receive(subject) {
+  }
+}
+
+const pipe = (funcs) => {
+  return funcs.reduce((f, g) => (...args) => f(g(...args)));
+}
+
+const curry = (func, preArgs = []) => {
+  const arity = func.length;
+  return function (...nextArgs) {
+    const args = [...preArgs, ...nextArgs];
+    if (args.length === arity) {
+      return func(...args);
+    }
+    return curry(func, args);
+  }
+}
+
+Promise.all = (promises) => {
   return new Promise((resolve, reject) => {
-    setTimeout(() => { 
-      func();
-      resolve();
-     }, wait);
+
   })
 }
+
+const sleep = (wait) => {
+  return new Promise((resolve, reject) => {
+    setTimeOut(() => {
+      resolve();
+    }, wait)
+  })
+}
+
+const requestPools = (urls, max = 3) => {
+  let completed = 0;
+  let progress = 0;
+  const res = []
+  const request = () => {
+    let i = progress;
+    progress++;
+    if (i > urls.length) {
+      return;
+    }
+    fetch(urls[i]).then(val => {
+      res[i] = val;
+    }).catch((e) => {
+      res[i] = e;
+    }).finally(() => {
+      completed++;
+      if (completed === urls.length) {
+        resolve(res);
+        return;
+      }
+      request();
+    })
+  }
+  while (progress < Math.min(urls.length, max)) {
+    request()
+  }
+}
+
+function red() {
+    console.log('red');
+}
+function green() {
+    console.log('green');
+}
+function yellow() {
+    console.log('yellow');
+}
+ 
+//红灯3秒亮一次，
+//黄灯2秒亮一次，
+//绿灯1秒亮一次；
+//如何让三个灯不断交替重复亮灯？（用Promise实现）三个亮灯函数已经存在：
+
+const light = (func, time) => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      func();
+      resolve();
+    }, time)
+  })
+}
+
 const step = () => {
   Promise.resolve().then(() => {
     return light(red, 3000);
@@ -26,87 +206,15 @@ const step = () => {
   }).then(() => {
     return light(yellow, 1000);
   }).then(() => {
-    step();
+    return step();
   })
 }
 step();
-Promise.resolveMy = (param) => {
-  if (param instanceof Promise) {
-    return param;
-  }
-  return new Promise((resolve, reject) => {
-    if (param.then && typeof param.then === 'function') {
-      param.then(resolve, reject);
-    } else {
-      resolve(param)
-    }
-  })
-}
 
-Promise.all = (promises) => {
-  return new Promise((resolve, reject) => {
-    let len = promises.length;
-    const res = [];
-    let completed = 0;
-    if (!res.length) {
-      resolve(res);
-      return;
-    }
-    for (let i = 0; i < len; i++) {
-      Promise.resolve(promises[i]).then(val => {
-        res[i] = val;
-        completed++;
-        if (completed === len) {
-          resolve(res);
-        }
-      }).catch(e => {
-        reject(e);
-      })
-    }
-  })
-}
-
-const concurrencyPromise = (urls, max) => {
-  return new Promise((resolve, reject) => {
-    const len = urls.length;
-    let completed = 0;
-    let current = 0
-    const res = [];
-    if (!len) {
-      resolve(res);
-      return;
-    }
-   
-    const request = () => {
-      let i = current;
-      current++;
-      if (i >= len) {
-        return;
-      }
-      fetch(urls[i]).then(val => {
-        res[i] = val;
-      }).catch(err => {
-        res[i] = err;
-      }).finally(() => {
-        completed++;
-        if (completed >= len) {
-          resolve(res);
-          return;
-        }
-        request();
-      })
-    }
-    while (current <= Math.min(max, urls.length)) {
-      request()
-    }
-  })
-}
-
-const newMy = function (cst, ...args) {
-  const newObj = Object.create(cst.prototype);
-  const res = cst.apply(newObj, args);
-  return typeof res === 'object' ? res : newObj;
-}
+/**
+ * 2024-6-18
+ * - 并发限制
+ */
 
 /**
  * 2024-6-13
